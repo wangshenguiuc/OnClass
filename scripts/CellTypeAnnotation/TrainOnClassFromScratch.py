5226bf5b1f91aa720c6eaa596017bdf653e18740
Sheng Wang <swang91@sh01-ln02.stanford.edu>
2020-03-04 16:51:12 -0800

    docs

--- scripts/CellTypeAnnotation/TrainOnClassFromScratch.py
+++ scripts/CellTypeAnnotation/TrainOnClassFromScratch.py
@@ -1,7 +1,7 @@
 from OnClass.utils import *
 from OnClass.OnClassModel import OnClassModel
 from OnClass.other_datasets_utils import my_assemble, data_names_all, load_names
-
+#train from scratch
 OnClassModel = OnClassModel()
 tp2emb, tp2i, i2tp = OnClassModel.EmbedCellTypes(dim=500,cell_type_network_file='../../../OnClass_data/cell_ontology/cl.ontology', use_pretrain='../../../OnClass_data/pretrain/tp2emb_500')
 print ('compute cell type embedding finished')
